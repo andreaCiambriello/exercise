421c184ba317848956b3a0b49be10d212523bec5
Andrea <and@gmail.com>
2022-12-02 18:57:38 +0100

    feat: added jsExercise_1.js

---
+++ jsExercise_1.js
@@ -0,0 +1,32 @@
+function calculateSalary(role) {
+    switch (role) {
+    case "ceo":
+     salary = 2200;
+     break;
+     case "manager":
+     salary = 1800;
+     break;
+     case "cto":
+     salary = 1800;
+     break;
+     case "developer":
+     salary = 1500;
+     break;
+     default:
+     salary = 1000;
+     break;
+  }
+  return salary;
+  }
+  
+  const ceoSalary = calculateSalary('ceo');
+  const managerSalary = calculateSalary('manager');
+  const ctoSalary = calculateSalary('cto');
+  const developerSalary = calculateSalary('developer');
+  const otherSalary = calculateSalary('other');
+  
+  console.log("The ceo salary is: " + ceoSalary +"€");
+  console.log("The manager salary is: " + managerSalary +"€");
+  console.log("The cto salary is: " + ctoSalary +"€");
+  console.log("The developer salary is: " + developerSalary +"€");
+  console.log("The other salary is: " + otherSalary +"€");

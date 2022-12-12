ff700fcb69ed8b168a9b70c66d76b420584935fa
Andrea <andrea.ciambriello@hotmail.com>
2022-12-12 19:36:11 +0100

    feat: added jsExercise_7

---
+++ jsExercise_7.js
@@ -0,0 +1,8 @@
+function printName() {
+    let helloName = "Hello John";
+    function inner() {
+        return helloName;
+    }
+    return inner;
+}
+setTimeout(() => console.log(printName()()), 1000);

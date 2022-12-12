0e6c19ad25af990bec8c44574fb85a77b86d5a5c
Andrea <andrea.ciambriello@hotmail.com>
2022-12-12 19:50:00 +0100

    feat: added jsExercise_9

---
+++ jsExercise_9.js
@@ -0,0 +1,14 @@
+const person = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  }
+  //keys
+  let keys = Object.keys(person);
+  console.log(keys);
+  //values
+  let values = Object.values(person);
+  console.log(values);
+  //key/value pairs
+  let entries = Object.entries(person);
+  console.log(entries);

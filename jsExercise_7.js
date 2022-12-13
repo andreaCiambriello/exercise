33afe48f629955758b69f88ca3131d1928bac77e
Andrea <andrea.ciambriello@hotmail.com>
2022-12-13 11:00:50 +0100

    fix: fixed jsExercise_7

--- jsExercise_7.js
+++ jsExercise_7.js
@@ -1,8 +1,24 @@
+//SOLUZIONE 1
+// function printName() {
+//     let helloName = "Hello John";
+//     function inner() {
+//         return helloName;
+//     }
+//     return inner;
+// }
+// setTimeout(() => console.log(printName()()), 1000);
+
+
+//SOLUZIONE 2
 function printName() {
     let helloName = "Hello John";
-    function inner() {
-        return helloName;
-    }
-    return inner;
+    setTimeout(function inner() {
+                console.log(helloName) }, 
+                1000);
 }
-setTimeout(() => console.log(printName()()), 1000);
+
+printName();
+
+//COMMENTO ALL'ESERCIZIO
+//entrambe le soluzioni funzionano, non sapendo quale delle due va bene, le carico entrambe
+//ho commentato la prima per non creare errori nel terminale

77852c5915aad5434306acb6501b185bc8ffa0a2
Andrea <andrea.ciambriello@hotmail.com>
2023-01-13 10:41:19 +0100

    feat: added jsExercise_38

---
+++ jsExercise_38.js
@@ -0,0 +1,30 @@
+const isLogged = true;
+
+function firstPromise(isLogged) {
+   return new Promise((resolve, reject) => {
+    let num = Math.random();
+        if(isLogged === true) {
+            resolve(num);
+        } else {
+            reject(new Error("Not logged in!"));
+        }
+    })
+}
+
+function secondPromise(num) {
+    console.log(num);                           //per vedere il numero random in console
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(num > 0.5) {
+                resolve(`{name: "John", age: 24}`);
+            } else {
+                reject(`error`);
+            }
+        }, 2000)
+    })
+}
+
+firstPromise(isLogged)
+    .then(secondPromise)
+    .then((val) => console.log(val))
+    .catch((err) => console.error(err))

0b71b868cda304e2eb35dc45d4bf968d461bf8d6
Andrea <andrea.ciambriello@hotmail.com>
2023-02-05 12:16:04 +0100

    feat: added react-exercise-7

---
+++ react-exercise-7/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDom from "react-dom"
+import { App } from "./App"
+
+const root = document.querySelector("#root");
+ReactDom.render(<App />, root);
+
+
+//App.js e Hello.js sono gli stessi dell'esercizio precedente, quindi linko solo l'index.js

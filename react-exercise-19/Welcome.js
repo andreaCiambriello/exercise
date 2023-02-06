d9ab842f569580406288ca708a88ba978f8465c6
Andrea <andrea.ciambriello@hotmail.com>
2023-02-06 21:59:46 +0100

    feat: added react-exercise-19

---
+++ react-exercise-19/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Age } from "./Age";
+
+export class Welcome extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome, {this.props.name ?? "Andrea"}!</p>
+                {this.props.age > 18
+                    ?<Age age={this.props.age}/>
+                    :<p>You are very young!</p>}
+            </div> 
+            
+        )
+    }
+}

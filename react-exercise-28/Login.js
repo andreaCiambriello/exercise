dbd63b6d7087d18816b385d6c7bef9b13c036bf7
Andrea <andrea.ciambriello@hotmail.com>
2023-02-10 11:13:32 +0100

    feat: added react-exercise-28

---
+++ react-exercise-28/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export class Login extends React.Component {
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleInputChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleLogin = () => {
+        this.props.onLogin(this.state)
+    }
+    
+    render() {
+        return (
+            <div>
+                <input name="username" type="text" value={this.state.username} onChange={this.handleInputChange}/>
+                <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange}/>
+                <button name="button" type="button" disabled={!this.state.username || !this.state.password} onClick={this.handleLogin}>Login</button>
+            </div>
+        )
+    }
+}

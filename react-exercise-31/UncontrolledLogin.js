29eb1e8a050b34c1acf0290eaa0e49e4a93cdd57
Andrea <andrea.ciambriello@hotmail.com>
2023-02-13 17:42:15 +0100

    feat: added react-exercise-31

---
+++ react-exercise-31/UncontrolledLogin.js
@@ -0,0 +1,37 @@
+import React, { createRef } from "react";
+
+export class UncontrolledLogin extends React.Component {
+    inputRef = createRef();
+    
+    handleFormSubmit = (event) => {
+        event.preventDefault()
+        
+        const username = event.target.elements.username.value
+        const password = event.target.elements.password.value
+        const remember = event.target.elements.remember.checked
+
+        console.log({
+            username,
+            password,
+            remember
+        })
+    }
+
+    componentDidMount () {
+        this.inputRef.current.focus();
+    }
+    
+    render() {
+        return (
+            <div>
+                <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
+                    <input name="username" type="text" ref={this.inputRef}/>
+                    <input name="password" type="password" />
+                    <input name="remember" type="checkbox" />
+                    <button name="button" type="submit">Login</button>
+                    <button type="reset">Reset</button>
+                </form>
+            </div>
+        )
+    }
+}

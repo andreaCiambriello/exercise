5feef2c54f81c6a73b2a6c734caa7408cf038790
Andrea <andrea.ciambriello@hotmail.com>
2023-02-19 14:37:39 +0100

    feat: added react-exercise-34

---
+++ react-exercise-34/TodoList.js
@@ -0,0 +1,42 @@
+import React, {createRef} from "react";
+
+export class TodoList extends React.Component {
+    listRef = createRef();
+
+    state = {
+        items: []
+    }
+
+    handleSendList = (event) => {
+        event.preventDefault()
+
+        const newItem = event.target.elements.list.value
+        if (newItem) {
+            this.setState((prevState) => ({
+                items: [...prevState.items, newItem]
+            }))
+            this.listRef.current.elements.list.value = ""
+        }
+    }
+
+    handleReset = () => {
+        this.setState(() => ({
+            items: []
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <form ref={this.listRef} onSubmit={this.handleSendList}>
+                    <input name="list" type="text"></input>
+                    <button name="button" type="submit">Send</button>
+                    <button type="button" onClick={this.handleReset}>Reset</button>
+                </form>
+                <ul>
+                    {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
+                </ul>
+            </div>
+        )
+    }
+}

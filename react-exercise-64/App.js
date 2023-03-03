577286c77373e80734ba3182a263402e4916f919
Andrea <andrea.ciambriello@hotmail.com>
2023-03-03 23:51:45 +0100

    feat: added react-exercise-64

---
+++ react-exercise-64/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Welcome} from "./Welcome";
+import { Routes, Route, Link } from "react-router-dom";
+import { ClickCounterFunction } from "./ClickCounterFunction";
+import { ShowGithubUser } from "./ShowGithubUser";
+
+export function App() {
+    return (
+        <div>
+            <div>
+                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="users/andreaCiambriello">My Github Profile Name</Link>
+            </div>
+            <Routes>
+                <Route path="/" element={<Welcome name="Andrea" />} />
+                <Route path="/counter" element={<ClickCounterFunction />} />
+                <Route path="users/:username" element={<ShowGithubUser />} />
+                <Route path="*" element={<p>Not Found</p>} />
+            </Routes>   
+        </div>
+        
+    )
+}

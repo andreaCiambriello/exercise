445fe39f5ae4970bb03b6794a81fa04ee68ceec5
Andrea <andrea.ciambriello@hotmail.com>
2023-03-04 14:24:24 +0100

    fix: fixed GithubUserList

--- react-exercise-65/GithubUserList.js
+++ react-exercise-65/GithubUserList.js
@@ -1,16 +1,32 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
 
 export function GithubUserList() {
+    const [usernames, setUsernames] = useState([])
+    const [newItem, setNewItem] = useState("")
+    
+    function handleNewItem (event) {
+        const newItem = event.target.value
+        setNewItem(newItem)
+    }
+    
+    function handleAddItem() {
+        setUsernames([...usernames, newItem])
+        setNewItem("")
+    }
 
     return (
         <div>
-            <Link to="/users/andreaCiambriello">Show andreaCiambriello name</Link>
+            <label>Search a user:</label>
             <br />
-            <br />
-            <Link to="/users/gianmarcotoso">Show gianmarcotoso name</Link>
-            <br />
-            <br />
-            <Link to="/users/malupp">Show malupp name</Link>
+            <input type="text" value={newItem} onChange={handleNewItem} />
+            <button onClick={handleAddItem}>Add User</button>
+
+            <ul>
+               {usernames.map((username, index) => <li key={index}>
+                   <Link to={`${username}`}>{`Show ${username} name`}</Link>
+                </li> )} 
+            </ul>
             <hr />
             <Outlet />
         </div>

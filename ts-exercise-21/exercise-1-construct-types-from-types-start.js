fd47c7183415d69004f81dcb86ed85816dc96ab2
Andrea <andrea.ciambriello@hotmail.com>
2023-03-10 21:43:18 +0100

    feat: added ts-exercise-21

---
+++ ts-exercise-21/exercise-1-construct-types-from-types-start.js
@@ -0,0 +1,33 @@
+/**
+ * START: Follow the instructions below.
+ */
+const countryA = {
+    code: "CN",
+    population: 1412600000,
+};
+const countryB = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+    currency: "Euro",
+    population: 10678632,
+};
+const countryC = {
+    name: "Italy",
+    code: "IT",
+    languages: ["Italian"],
+    population: 60317116,
+};
+console.log(countryC);
+const countryD = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5135300,
+};
+const countryE = {
+    name: "Thailand",
+    code: "TH",
+    languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
+    currency: "Baht",
+};
+export {};

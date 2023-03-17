396fbb8b380d0c1b4df9a24132506e707cf894da
Andrea <andrea.ciambriello@hotmail.com>
2023-03-17 18:37:35 +0100

    feat: added exercise11

---
+++ exercise11/script.mjs
@@ -0,0 +1,21 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  };
+
+luckyDraw("Joe")
+  .then((res) => console.log(res))
+  .then(() => luckyDraw("Caroline"))
+  .then((res2) => console.log(res2))
+  .then(() => luckyDraw("Sabrina"))
+  .then((res3) => console.log(res3))
+  .catch((err) => console.error(err));
